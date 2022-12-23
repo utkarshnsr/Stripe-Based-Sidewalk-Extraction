789df42c79dba7e56e53c1ca7cdf14c54623f8a7
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2022-12-23 11:50:40 -0500

    removed example.py file

--- example.py
+++
@@ -1,7 +0,0 @@
-import open3d as o3d
-import numpy as np
-
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(np.array([[1,2,3],[4,5,6]]))
-pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-o3d.visualization.draw_geometries([pcd])
